96f490c5c890a8f44008e59be8a52ee4ddbf2c0b
abdo adel <abdo48439@gmail.com>
2023-04-17 12:16:57 +0200

    edite files

--- js/script.js
+++ js/script.js
@@ -10,6 +10,29 @@ const hueRotate = document.getElementById('hue-rotate');
 const download = document.getElementById('download');
 const reset = document.querySelector('.btn-danger');
 
+// reset Value when upload new img
+function resetValue(){
+img.style.filter = 'none';
+saturate.value = '100';
+contrast.value = '100';
+brightness.value = '100';
+sepia.value = '0';
+grayscale.value = '100';
+blur.value = '0';
+hueRotate.value ='0';
+}
+// reset value when click on reset button
+reset.addEventListener('click',() => {
+saturate.value = '100';
+contrast.value = '100';
+brightness.value = '100';
+sepia.value = '0';
+grayscale.value = '100';
+blur.value = '0';
+hueRotate.value ='0';
+img.style.filter = 'none';
+
+})
 // hide the button reset, download when loading the page \
 window.onload = function(){
     // get the variables and add style none
@@ -20,6 +43,7 @@ window.onload = function(){
 // 1- get the variable upload 
 // 2- onchange - then show the hiding buttons
 upload.onchange = function(){
+    resetValue();
     download.style.display = 'block';
     reset.style.display = 'block';
     // read the file 
@@ -29,3 +53,33 @@ upload.onchange = function(){
         img.src = file.result;
     }
 }
+//======================================
+let filters = document.querySelectorAll('ul li input');
+filters.forEach(filter => {
+    filter.addEventListener('input', function(){
+        img.style.filter = `
+        saturate(${saturate.value}%)
+        contrast(${contrast.value}%)
+        brightness(${brightness.value}%)
+        sepia(${sepia.value}%)
+        grayscale(${grayscale.value})
+        blur(${blur.value}px)
+        hue-rotate(${hueRotate.value}deg)
+        `
+    })
+}) 
+
+//============================================
+// download 
+download.addEventListener('click', () => {
+   let canvas = document.createElement('canvas');
+   canvas.width = img.naturalWidth; 
+   canvas.height = img.naturalHeight; 
+   let ctx = canvas.getContext('2d');
+   ctx.filter = img.style.filter;
+   ctx.drawImage(img,0,0);
+   let link = document.createElement('a');
+   link.download = 'filtered-img.png'
+   link.href = canvas.toDataURL();
+   link.click();
+})
